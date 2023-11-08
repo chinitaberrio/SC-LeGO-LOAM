3e76367c0b7807cd832d45c25d9443239eccd189
chinitaberrio <chinitaberrio@gmail.com>
2023-11-08 11:38:40 +1100

    ouster 128 beams config

--- SC-LeGO-LOAM/LeGO-LOAM/include/utility.h
+++ SC-LeGO-LOAM/LeGO-LOAM/include/utility.h
@@ -98,12 +98,21 @@ extern const bool useCloudRing = false; // if true, ang_res_y and ang_bottom are
 // extern const int groundScanInd = 7;
 
 // Ouster OS1-64
-extern const int N_SCAN = 64;
+//extern const int N_SCAN = 64;
+//extern const int Horizon_SCAN = 1024;
+//extern const float ang_res_x = 360.0/float(Horizon_SCAN);
+//extern const float ang_res_y = 33.2/float(N_SCAN-1);
+//extern const float ang_bottom = 16.6+0.1;
+//extern const int groundScanInd = 15;
+
+// Ouster OS1-128
+extern const int N_SCAN = 128
 extern const int Horizon_SCAN = 1024;
 extern const float ang_res_x = 360.0/float(Horizon_SCAN);
-extern const float ang_res_y = 33.2/float(N_SCAN-1);
-extern const float ang_bottom = 16.6+0.1;
-extern const int groundScanInd = 15;
+extern const float ang_res_y = 45/float(N_SCAN-1);
+extern const float ang_bottom = 22.5;
+extern const int groundScanInd = 9;
+
 
 extern const bool loopClosureEnableFlag = true;
 extern const double mappingProcessInterval = 0.3;
